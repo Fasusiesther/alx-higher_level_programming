18255b57399aaf0f78c1d5d90759988dc7c4bb0a
Fasusiesther <fasusi4esther@yahoo.com>
2022-11-29 10:14:02 -0800

    Task 13 and 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for b in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(b, chr(b - 33)), end="")
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(i - diff)), end='')
