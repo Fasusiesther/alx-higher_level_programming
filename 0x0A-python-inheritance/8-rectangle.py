8209e89c789eaaecadf88a01dbacb404ceeacd6f
Fasusiesther <fasusi4esther@yahoo.com>
2023-01-09 04:41:03 -0800

    Task 8-12

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-"""public instance method"""
+"""inherits from BaseGeometry"""
 
 
 class BaseGeometry:
     """public instance method"""
+
     def area(self):
         """raises an exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """validates value"""
-        x = isinstance(value, int)
-        if not x:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initialization of privates"""
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
