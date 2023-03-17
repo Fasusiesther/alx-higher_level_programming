507f2c35cf9892d8f8849ba9e3f17a55b1350b50
Fasusiesther <fasusi4esther@yahoo.com>
2023-03-17 04:45:17 -0700

    Task 6-9

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""Lists all State objects from the database hbtn_0e_6_usa"""
+
+
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
+if __name__ == '__main__':
+    URL = 'mysql+mysqldb://{}:{}@localhost/{}'
+    s1, s2, s3 = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(URL.format(s1, s2, s3), pool_pre_ping=True)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
-"""
+"""Lists all State objects from the database hbtn_0e_6_usa"""
+
+
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
+if __name__ == '__main__':
+    URL = 'mysql+mysqldb://{}:{}@localhost/{}'
+    s1, s2, s3 = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(URL.format(s1, s2, s3), pool_pre_ping=True)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
+
+    state = session.query(State).first()
+    if state is None:
+        print('Nothing')
     else:
-        print(instance.id, instance.name, sep=": ")
+        print(f'{state.id}: {state.name}')
